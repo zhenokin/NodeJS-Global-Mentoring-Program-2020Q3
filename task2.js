ee47bb90bc7a743f714aae14b78cf59114e3b158
Zhenokin <zhenokin.lida@gmail.com>
2020-08-09 11:06:21 +0300

    Complete task 1.2

---
+++ task2.js
@@ -0,0 +1,13 @@
+const csvtojson = require("csvtojson");
+const fs = require('fs');
+
+const csvFile = './csv/nodejs-hw1-ex1.csv';
+const txtFile = './txt/test.txt';
+
+const readStream = fs.createReadStream(csvFile);
+const writeStream = fs.createWriteStream(txtFile);
+
+readStream.pipe(csvtojson())
+    .on('error', (err) => console.error(err))
+    .pipe(writeStream)
+    .on('error', (err) => console.error(err));
